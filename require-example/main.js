7ca7cdc9f803915d2309249f4d3a0b78746dd3ae
Roy Peled <peled.roy@gmail.com>
2018-07-10 16:54:52 +0300

    more changes

--- require-example/main.js
+++ require-example/main.js
@@ -1,4 +1,4 @@
 var figlet = require("figlet");
 const cli = require('ascli')('cr');
 
-cli.log(figlet.textSync('Hello Class', 'Computer').cyan);
+cli.log(figlet.textSync('Hello Class', 'doom').green);
